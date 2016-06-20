5ca498b20719c521275dbf6947525772fcada9d0
kjayasa <kjayasa@users.noreply.github.com>
2016-06-20 12:02:22 -0400

    added filtring

--- src/resx-parser.js
+++ src/resx-parser.js
@@ -4,16 +4,18 @@ import changeCase from 'change-case'
 
 export default class ResxParser {
   constructor({
-    convertIdCase = 'camel',    // camel,constant,dot,header,isLower,isUpper,lower,lcFirst,no,param,pascal,path,sentence,snake,swap,title,upper,ucFirst
-      fnTransformId = null,     // (idString)=>{  doSometing(idString) ... return idString;}
-      fnTransformValue = null   // (valueString)=>{  doSometing(valueString) ... return valueString;
+    convertIdCase = 'camel', // camel,constant,dot,header,isLower,isUpper,lower,lcFirst,no,param,pascal,path,sentence,snake,swap,title,upper,ucFirst
+      fnTransformId = null, // (idString)=>{  doSometing(idString) ... return idString;}
+      fnTransformValue = this.transformResource, // (valueString)=>{  doSometing(valueString) ... return valueString;
+      filter = null
   } = {}) {
 
     this.convertIdCase = convertIdCase;
     this.fnTransformId = fnTransformId;
     this.fnTransformValue = fnTransformValue;
+    this.filter = filter;
   }
-  
+
   do(fn, callback) {
     let that = this;
     return function(err, ...args) {
@@ -42,17 +44,46 @@ export default class ResxParser {
   }
 
   extractFromXml(xmlDom, callback) {
-    let ret = {};
+    let resources = {};
     if (xmlDom && xmlDom.root && xmlDom.root.data && xmlDom.root.data.length > 0) {
-      xmlDom.root.data.forEach((node) => ret[this.getID(node)] = this.getValue(node));
-    }
-    if (callback) {
-      callback(null, ret);
+      xmlDom.root.data.forEach((node) => {
+        let res = this.getResource(node);
+        if (res) {
+          resources[res.id] = res.resource;
+        }
+      });
+
+      if (callback) {
+        callback(null, resources);
+      }
     }
   }
 
-  getID(item) {
-    let id = item.$.name;
+  getResource(item) {
+    if (item && item.$ && item.$.name) {
+
+      let id = this.getID(item.$.name),
+        value = item.value && item.value[0] ? item.value[0] : "",
+        comment = item.comment && item.comment[0] ? item.comment[0] : "";
+
+      if (this.filter) {
+        if (this.filter(id, value, comment)) {
+          return null;
+        }
+
+      }
+      return {
+        id: id,
+        resource: this.fnTransformValue(id, value, comment)
+      };
+    }
+  }
+
+  transformResource(id, value, comment) {
+    return value;
+  }
+
+  getID(id) {
     if (this.convertIdCase) {
       id = changeCase[this.convertIdCase](id);
     }
@@ -62,12 +93,26 @@ export default class ResxParser {
     return id;
   }
 
-  getValue(item) {
-    let value = item.value[0];
-    if (this.fnTransformValue) {
-      value = this.fnTransformValue(value);
-    }
-    return value;
+
+  /* TODO: 
+  //  error checking
+  // get comment
+  // return{ 'ID':{value:"value",comment:"comment"}}
+  // options{
+    convertIdCase:
+    filter:function(id,value,comment){return true},
+    processValue:function(id,value,comment){return value},
+    processId:function(id){return id}
+
   }
+  */
+
+  /* getComment(item) {
+     let value = item.comment[0] || "";
+     if (this.fnTransformValue) {
+       value = this.fnTransformValue(value);
+     }
+     return value;
+   }*/
 
 }
