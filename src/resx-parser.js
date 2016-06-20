1a20b25a1361bf2e14b146aa5dfeb1daa74789af
kjayasa <kjayasa@users.noreply.github.com>
2016-06-20 13:15:00 -0400

    more test cases

--- src/resx-parser.js
+++ src/resx-parser.js
@@ -1,16 +1,24 @@
+import 'babel-polyfill'
 import xml2js from 'xml2js'
 import fs from 'fs'
 import changeCase from 'change-case'
 
 export default class ResxParser {
   constructor({
-    convertIdCase = 'camel', // camel,constant,dot,header,isLower,isUpper,lower,lcFirst,no,param,pascal,path,sentence,snake,swap,title,upper,ucFirst
-      fnTransformId = null, // (idString)=>{  doSometing(idString) ... return idString;}
-      fnTransformValue = this.transformResource, // (valueString)=>{  doSometing(valueString) ... return valueString;
-      filter = null
+    convertIdCase = 'camel', // camel,constant,dot,header,lower,lcFirst,no,param,pascal,path,sentence,snake,swap,title,upper,ucFirst
+    fnTransformId = null, // (idString)=>{  doSometing(idString) ... return idString;}
+    fnTransformValue = this.transformResource, // (valueString)=>{  doSometing(valueString) ... return valueString;
+    filter = null
   } = {}) {
+    const supportedCases = ["camel","constant","dot","header","lower","lcFirst","no","param","pascal","path","sentence","snake","swap","title","upper","ucFirst"];
 
-    this.convertIdCase = convertIdCase;
+    if (convertIdCase) {
+      if (supportedCases.includes(convertIdCase) == true) {
+        this.convertIdCase = convertIdCase;
+      } else {
+        throw new Error(`${convertIdCase} is not a supported case conversion`);
+      }
+    }
     this.fnTransformId = fnTransformId;
     this.fnTransformValue = fnTransformValue;
     this.filter = filter;
@@ -45,16 +53,22 @@ export default class ResxParser {
 
   extractFromXml(xmlDom, callback) {
     let resources = {};
-    if (xmlDom && xmlDom.root && xmlDom.root.data && xmlDom.root.data.length > 0) {
-      xmlDom.root.data.forEach((node) => {
-        let res = this.getResource(node);
-        if (res) {
-          resources[res.id] = res.resource;
-        }
-      });
+    try {
+      if (xmlDom && xmlDom.root && xmlDom.root.data && xmlDom.root.data.length > 0) {
+        xmlDom.root.data.forEach((node) => {
+          let res = this.getResource(node);
+          if (res) {
+            resources[res.id] = res.resource;
+          }
+        });
 
+        if (callback) {
+          callback(null, resources);
+        }
+      }
+    } catch (ex) {
       if (callback) {
-        callback(null, resources);
+        callback(ex, null)
       }
     }
   }
@@ -76,6 +90,8 @@ export default class ResxParser {
         id: id,
         resource: this.fnTransformValue(id, value, comment)
       };
+    } else {
+      throw new Error("Resource Id missing");
     }
   }
 
@@ -92,27 +108,4 @@ export default class ResxParser {
     }
     return id;
   }
-
-
-  /* TODO: 
-  //  error checking
-  // get comment
-  // return{ 'ID':{value:"value",comment:"comment"}}
-  // options{
-    convertIdCase:
-    filter:function(id,value,comment){return true},
-    processValue:function(id,value,comment){return value},
-    processId:function(id){return id}
-
-  }
-  */
-
-  /* getComment(item) {
-     let value = item.comment[0] || "";
-     if (this.fnTransformValue) {
-       value = this.fnTransformValue(value);
-     }
-     return value;
-   }*/
-
 }
