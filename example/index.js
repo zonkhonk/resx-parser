c7a4837d7d5546c0eeca43a5b540a517d3b243c9
kjayasa <kjayasa@users.noreply.github.com>
2016-06-20 12:37:56 -0400

    more examples and tests

--- example/index.js
+++ example/index.js
@@ -1,4 +1,24 @@
-(function resxParserUsage(ResxParser, fs) {
+(function resxParserUsage(ResxParser, fs, q) {
+
+  var xmlString =
+    '<root>' +
+    '<data name="Arm-Button" xml:space="preserve">' +
+    '<value>Arm</value>' +
+    '<comment>text on the arm button</comment>'+
+    '</data>' +
+    '<data name="CoordinatesLabel" xml:space="preserve">' +
+    '<value>Enter coordinates</value>' +
+    '<comment>text on the label for coordinates</comment>'+
+    '</data>' +
+    '<data name="launchButton" xml:space="preserve">' +
+    '<value>Launch</value>' +
+    '<comment>text on the launch button</comment>'+
+    '</data>' +
+    '<data name="title" xml:space="preserve">' +
+    '<value>nuke launch controls</value>' +
+    '<comment>title of the console</comment>'+
+    '</data>' +
+    '</root>';
 
   function logResult(err, result) {
     if (err) {
@@ -8,15 +28,48 @@
     }
   }
 
-  function parseFile() {
+  function parseString() {
+    // init parser with default options
+    var parser = new ResxParser();
+    parser.parseString(xmlString, logResult);
+  }
 
+  function filter() {
+    var options = {
+      filter: function(id, value, comment) {
+        if (id === "title") { 
+          return false;
+        } else {
+          return true;
+        }
+      }
+    };
+    var parser = new ResxParser(options);
+    parser.parseString(xmlString, logResult);
+  }
+
+   function transformOutput() {
+    var options = {
+      fnTransformValue: function (id, value, comment) { // will be called for each resource id
+        return {
+          value:value,
+          comment:comment
+        };
+      }
+    };
+
+    var parser = new ResxParser(options);
+    parser.parseString(xmlString, logResult);
+  }
+
+  function parseFile() {
     // various options for preprossing
     var options = {
       convertIdCase: 'snake', // camel,constant,dot,header,isLower,isUpper,lower,lcFirst,no,param,pascal,path,sentence,snake,swap,title,upper,ucFirst
       fnTransformId: function(id) { // will be called for each resource id
         return "RESOURCE_" + id;
       },
-      fnTransformValue: function(value) { // will be called for each resource id
+      fnTransformValue: function (id, value, comment) { // will be called for each resource id
         if (value) {
           return value.toUpperCase();
         }
@@ -28,33 +81,14 @@
 
   }
 
-  function parseString() {
-    var xmlString =
-      '<root>' +
-      '<data name="Arm-Button" xml:space="preserve">' +
-      '<value>Arm</value>' +
-      '</data>' +
-      '<data name="CoordinatesLabel" xml:space="preserve">' +
-      '<value>Enter coordinates</value>' +
-      '</data>' +
-      '<data name="launchButton" xml:space="preserve">' +
-      '<value>Launch</value>' +
-      '</data>' +
-      '<data name="title" xml:space="preserve">' +
-      '<value>nuke launch controls</value>' +
-      '</data>' +
-      '</root>';
-
-    // init parser with default options
-    var parser = new ResxParser();
-
-    parser.parseString(xmlString, logResult);
-  }
-
   console.log("\nparsing string\n");
   parseString();
+  console.log("\nparsing and filtering \n");
+  filter();
+  console.log("\ntransforming output");
+  transformOutput();
   console.log("\nparsing file\n");
   parseFile();
 
 
-})(require('../lib/resx-parser'), require('fs'));
+})(require('../lib/resx-parser'), require('fs'),require('q'));
--- spec/resx-parser.spec.js
+++ spec/resx-parser.spec.js
@@ -4,15 +4,19 @@ describe("resx-parese", function() {
     '<root>' +
     '<data name="Arm-Button" xml:space="preserve">' +
     '<value>Arm</value>' +
+    '<comment>text on the arm button</comment>'+
     '</data>' +
     '<data name="CoordinatesLabel" xml:space="preserve">' +
     '<value>Enter coordinates</value>' +
+    '<comment>text on the label for coordinates</comment>'+
     '</data>' +
     '<data name="launchButton" xml:space="preserve">' +
     '<value>Launch</value>' +
+    '<comment>text on the launch button</comment>'+
     '</data>' +
     '<data name="title" xml:space="preserve">' +
     '<value>nuke launch controls</value>' +
+    '<comment>title of the console</comment>'+
     '</data>' +
     '</root>';
 
@@ -56,15 +60,59 @@ describe("resx-parese", function() {
     it("should use ID 'case transformation' if supplied in options", function(done) {
 
       var expected = {
-        'Arm Button': 'Arm',
-        'Coordinates Label': 'Enter coordinates',
-        'Launch Button': 'Launch',
-        'Title': 'nuke launch controls'
-      },
-      options = {
-        convertIdCase: 'title'
-      },
-      parser = new ResxParser(options);
+          'Arm Button': 'Arm',
+          'Coordinates Label': 'Enter coordinates',
+          'Launch Button': 'Launch',
+          'Title': 'nuke launch controls'
+        },
+        options = {
+          convertIdCase: 'title'
+        },
+        parser = new ResxParser(options);
+
+      parser.parseString(xmlString, function(err, result) {
+        expect(result).toEqual(expected);
+        done();
+      });
+    });
+
+    it("should use filter if supplied in options", function(done) {
+
+      var expected = {
+          'title': 'nuke launch controls'
+        },
+        options = {
+          filter: function(id, value, comment) {
+
+            if (id === "title") {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        },
+        parser = new ResxParser(options);
+
+      parser.parseString(xmlString, function(err, result) {
+        expect(result).toEqual(expected);
+        done();
+      });
+    });
+
+    it("should be able to transform output by using fnTransformValue ", function(done) {
+
+      var expected = {
+          armButton: ['Arm', 'text on the arm button'],
+          coordinatesLabel: ['Enter coordinates', 'text on the label for coordinates'],
+          launchButton: ['Launch', 'text on the launch button'],
+          title: ['nuke launch controls', 'title of the console']
+        },
+        options = {
+          fnTransformValue: function(id, value, comment) {
+            return [value, comment];
+          }
+        },
+        parser = new ResxParser(options);
 
       parser.parseString(xmlString, function(err, result) {
         expect(result).toEqual(expected);
